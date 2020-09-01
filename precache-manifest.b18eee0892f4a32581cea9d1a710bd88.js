7f0aa0bea84ab516c04b15d24688aa31b63dc222
Elena-brv <leena.brv@gmail.com>
2020-09-01 21:01:48 +0300

    Updates

---
+++ precache-manifest.b18eee0892f4a32581cea9d1a710bd88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a6ec159c8c0664e47e0df963cbcdf9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a00ddb7360a20de253e",
+    "url": "./static/css/main.08bfd160.chunk.css"
+  },
+  {
+    "revision": "36689209a43d29ff1e8c",
+    "url": "./static/js/2.d4c7f416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d4c7f416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a00ddb7360a20de253e",
+    "url": "./static/js/main.2fab5adc.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  },
+  {
+    "revision": "17d0b0a1a2ba9d26444b0339836bc2d5",
+    "url": "./static/media/wooden-boards.17d0b0a1.jpg"
+  }
+]);
